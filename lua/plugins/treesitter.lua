e94f5f2e440b63e431f5e50a3a58961fec8986de
thonycsdev <thonycout@gmail.com>
2024-03-22 03:35:46 -0300

    Add go support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		local configTreeSitter = require("nvim-treesitter.configs")
 
 		configTreeSitter.setup({
-			ensure_installed = { "lua", "javascript", "c_sharp" },
+			ensure_installed = { "lua", "javascript", "c_sharp", "go" },
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
